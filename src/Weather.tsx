28b7b570ab0a57b3db4e4d19bfaa69bb106fb39c
Topi Saavalainen <topi.saavalainen@gmail.com>
2023-06-11 19:55:47 +0300

    Added weather icons

--- src/Weather.tsx
+++ src/Weather.tsx
@@ -1,27 +1,24 @@
 import { useEffect, useState } from "react";
 import * as secrets from '../secrets.json'
-
-interface Forecast {
-    time: string,
-    temp: number,
-    weatherId: number,
-    weather: string,
-    gust: number,
-}
+import { Forecast, OWMForecast, OWMWeather } from "./interfaces";
+import './assets/weather-css/weather-icons.min.css';
 
 export default function Weather() {
     const [currentTemp, setCurrentTemp] = useState(0)
+    const [currentWeather, setCurrentWeather] = useState('')
     const [forecast, setForecast] = useState<Forecast[]>([])
 
     function fetchWeather() {
         const weatherUrl = 'http://api.openweathermap.org/data/2.5/weather';
         const cityId = 633679;
+        const time = Math.round(new Date().getTime() / 1000);
         fetch(weatherUrl + '?id=' + cityId + '&appid=' + secrets.OpenWeatherAppId + '&units=metric')
             .then(response => {
                 return response.json();
             })
-            .then(data => {
+            .then((data: OWMWeather) => {
                 setCurrentTemp(data.main.temp)
+                setCurrentWeather(getWeatherIconClass(data.weather[0].id, time, data.sys.sunset, data.sys.sunrise))
             })
     }
 
@@ -37,16 +34,86 @@ export default function Weather() {
             })
     }
 
-    function getForecast(data): Forecast[] {
+    function getForecast(data: OWMForecast): Forecast[] {
+        const sunrise = data.city.sunrise;
+        const sunset = data.city.sunset;
         return data.list.map((d) => (
             {
                 time: d.dt_txt.slice(-8, -3),
                 temp: Math.round(d.main.temp),
                 weatherId: d.weather[0].id,
                 weather: d.weather[0].main,
-                gust: d.wind.gust
+                gust: d.wind.gust,
+                iconClass: getWeatherIconClass(d.weather[0].id, d.dt, sunset, sunrise)
             }
         )).slice(0, 5)
+    }
+
+    function getWeatherIconClass(id:number, iconTime:number, sunset:number, sunrise:number) {
+        if (typeof(id) != 'number') {
+          return 'wi-alien';
+        }
+        const weatherId = id.toString().slice(0,1)
+        let weatherClass: string;
+        const nextSunrise = sunrise + 24 * 60 * 60;
+        
+        const daytime = (iconTime > sunrise  && iconTime < sunset) || iconTime > nextSunrise;
+    
+        switch (weatherId) {
+          case '2':
+            weatherClass = 'wi-thunderstorm';
+            break;
+          case '3':
+            weatherClass = 'wi-showers';
+            break;
+          case '5':
+            weatherClass = 'wi-rain';
+            break;
+          case '6':
+            weatherClass = 'wi-snow';
+            break;
+          case '7':
+            weatherClass = 'wi-dust';
+            break;
+          case '8':
+            switch (id) {
+              case 801:
+                if (daytime) {
+                  weatherClass = 'wi-day-cloudy';
+                }
+                else {
+                  weatherClass = 'wi-night-alt-cloudy';
+                }
+                break;
+              case 802:
+                if (daytime) {
+                  weatherClass = 'wi-day-cloudy';
+                }
+                else {
+                  weatherClass = 'wi-night-alt-cloudy';
+                }
+                break;
+              case 803:
+                weatherClass = 'wi-cloud';
+                break;
+              case 804:
+                weatherClass = 'wi-cloudy';
+                break;
+              default:
+                if (daytime) {
+                    weatherClass = 'wi-day-sunny';
+                }
+                else {
+                  weatherClass = 'wi-night-clear';
+                }
+            }
+            break;
+    
+          default: 
+          weatherClass = 'wi-alien';
+        }
+    
+        return weatherClass;
     }
 
     useEffect(() => {
@@ -56,12 +123,15 @@ export default function Weather() {
 
     return (
         <div className="weather">
-            <div className="current-temp">{Math.round(currentTemp)}°</div>
+            <div className="current-weather">
+                <i className={`wi ${currentWeather}`}></i>
+                <span className="current-temp">{Math.round(currentTemp)}°</span>
+            </div>
             <ul className="forecast">
                 {forecast.map((f, i) => (
                     <li key={i}>
                         <div className="temp">{f.temp}°</div>
-                        <div className="weather">{f.weather}</div>
+                        <div className="weather"><i className={`wi ${f.iconClass}`}></i></div>
                         <div className="time">{f.time}</div>
                     </li>
                 ))}
---
+++ src/interfaces.ts
@@ -0,0 +1,131 @@
+export interface OWMWeather {
+    coord: Coord;
+    weather: Weather[];
+    base: string;
+    main: Main;
+    visibility: number;
+    wind: Wind;
+    clouds: Clouds;
+    dt: number;
+    sys: Sys;
+    timezone: number;
+    id: number;
+    name: string;
+    cod: number;
+  }
+  
+  interface Sys {
+    type: number;
+    id: number;
+    country: string;
+    sunrise: number;
+    sunset: number;
+  }
+  
+  interface Clouds {
+    all: number;
+  }
+  
+  interface Wind {
+    speed: number;
+    deg: number;
+  }
+  
+  interface Main {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+  }
+  
+  interface Weather {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }
+  
+  interface Coord {
+    lon: number;
+    lat: number;
+  }
+
+export interface OWMForecast {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: List[];
+  city: City;
+}
+
+interface City {
+  id: number;
+  name: string;
+  coord: Coord;
+  country: string;
+  population: number;
+  timezone: number;
+  sunrise: number;
+  sunset: number;
+}
+
+interface Coord {
+  lat: number;
+  lon: number;
+}
+
+interface List {
+  dt: number;
+  main: Main;
+  weather: Weather[];
+  clouds: Clouds;
+  wind: Wind;
+  visibility: number;
+  pop: number;
+  sys: Sys;
+  dt_txt: string;
+}
+
+interface Sys {
+  pod: string;
+}
+
+interface Wind {
+  speed: number;
+  deg: number;
+  gust: number;
+}
+
+interface Clouds {
+  all: number;
+}
+
+interface Weather {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+}
+
+interface Main {
+  temp: number;
+  feels_like: number;
+  temp_min: number;
+  temp_max: number;
+  pressure: number;
+  sea_level: number;
+  grnd_level: number;
+  humidity: number;
+  temp_kf: number;
+}
+
+export interface Forecast {
+    time: string,
+    temp: number,
+    weatherId: number,
+    weather: string,
+    gust: number,
+    iconClass: string,
+}
